f9701c0f03e17c7d6fbf40d3a16cf0295defb817
bkeuria1 <benkeurian@gmail.com>
2022-05-24 12:46:15 -0400

    Added new Env variables

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Button,Card } from 'react-bootstrap'
 const Home = ()=>{
     let signInURL
-    if(process.env.REACT_APP_MODE = 'dev'){
+    if(process.env.REACT_APP_MODE === 'dev'){
         signInURL = process.env.REACT_APP_SIGN_IN_URL_DEV
-    }else if(process.env.REACT_APP_MODE = 'production'){
+    }else if(process.env.REACT_APP_MODE === 'production'){
         signInURL = process.env.REACT_APP_SIGN_IN_URL_PRODUCTION
     }
     return(
